411a7c1ac2af82ed201ee224154a8ed74ef78174
Mo Li <selinamoli88@gmail.com>
2023-05-18 11:52:24 -0700

    some simple code

--- cpp primer/main.cpp
+++ cpp primer/main.cpp
@@ -7,8 +7,25 @@
 
 #include <iostream>
 
+void bucks(double);
+
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    using namespace std;
+    cout << "Hello, World!\n";
+    cout << "lalala";
+    cout << endl;
+    bucks(1234.56);
+    cout << endl;
+    cout << "max int is " << INT_MAX << endl;
+    float a = 2.34E+6;
+    float b = a + 1;
+    cout << "difference " << b - a << endl;
+    char grade = 65;
+    cout << "grade is " << grade << endl;
     return 0;
+}
+
+void bucks(double n) {
+    std::cout << n;
 }
